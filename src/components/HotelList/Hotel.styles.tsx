f6d8d1744cfd9272f0fa8780c081bf361ac3d53a
Annie Huang <annie.hn@gmail.com>
2023-09-04 13:00:37 +1000

    Mobile vs Tablets vs Desktop views

--- src/components/HotelList/Hotel.styles.tsx
+++ src/components/HotelList/Hotel.styles.tsx
@@ -4,27 +4,48 @@ export const HotelWrapper = styled.li`
   --spacing: 10px;
   margin: var(--spacing) 0;
   display: flex;
+  flex-direction: column;
+  gap: var(--spacing);
+  padding: calc(var(--spacing) * 2) calc(var(--spacing) * 2) 0;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    padding: 0 calc(var(--spacing) * 2);
+    gap: 0;
+  }
 `;
 
 export const Image = styled.img`
   width: 200px;
   aspect-ratio: 1;
   margin-right: 20px;
-  padding-bottom: var(--spacing);
+
+  @media (min-width: 768px) {
+    padding-bottom: var(--spacing);
+  }
 `;
 
 export const HotelInfo = styled.div`
-  border-bottom: 1px solid lightgray;
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  @media (min-width: 768px) {
+    border-bottom: 1px solid lightgray;
+  }
 `;
 
 export const Headline = styled.div`
   display: flex;
   align-items: baseline;
   gap: var(--spacing);
+  flex-direction: column;
+
+  // only make it one liner when it is in desktop
+  @media (min-width: 976px) {
+    flex-direction: row;
+  }
 `;
 
 export const Heading = styled.h2`
@@ -33,10 +54,12 @@ export const Heading = styled.h2`
   // Make the ... ellipsis when it reaches certain size
   // Use max-width instead of min-width because we need to make rating component next to the text when it is short
   // width: 414px;
-  max-width: 414px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+  @media (min-width: 768px) {
+    max-width: 414px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `;
 
 export const Address = styled.address`
@@ -57,19 +80,26 @@ export const Note = styled.div`
 export const HotelPrice = styled.div`
   border-bottom: 1px solid lightgray;
   white-space: nowrap;
-  text-align: end;
-  padding-top: 40px;
+  padding-bottom: var(--spacing);
+
+  @media (min-width: 768px) {
+    text-align: end;
+    padding-top: 40px;
+  }
 `;
 
 export const DisplayPrice = styled.div`
   font-size: 35px;
   display: flex;
-  justify-content: end;
 
   span {
     font-size: 20px;
     font-weight: bold;
     padding-top: 3px;
+  }
+
+  @media (min-width: 768px) {
+    justify-content: end;
   }
 `;
 
