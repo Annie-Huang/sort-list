a99e7d1c13f4fc3a264901fb410b3ae338ee5dfa
Annie Huang <annie.hn@gmail.com>
2023-09-07 15:22:14 +1000

    Create <h1> in landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ export default function App() {
   return (
     <Container>
       <Logo src='./qantas-logo.png' alt='qantas logo' />
+      {/* A page should have a h1 before using h2 (in HotelList component) for vision impaired users */}
+      <h1 className='sr-only'>Qantas hotels page</h1>
       <Summary>
         <div>
           <strong>{hotelList.length}</strong>
