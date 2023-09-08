78d28cb477ee97bfc04959fc14f6f968af50ac25
Annie Huang <annie.hn@gmail.com>
2023-09-08 13:37:08 +1000

    setupTest to mock fetch call globally

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,23 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import jsonData from '../_data/data.json';
+
+// https://kentcdodds.com/blog/stop-mocking-fetch
+async function mockFetch(url, config) {
+  switch (url) {
+    case 'http://localhost:4000/results': {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => jsonData.results,
+      };
+    }
+    default: {
+      throw new Error(`Unhandled request: ${url}`);
+    }
+  }
+}
+
+beforeAll(() => jest.spyOn(window, 'fetch'));
+beforeEach(() => window.fetch.mockImplementation(mockFetch));
